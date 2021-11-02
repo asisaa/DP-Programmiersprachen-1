36b3cbbe1c63f4b5a1188b0e4450ae6602556327
Asisa Asseily <asisaasseily@asisas-mbp.lan>
2021-11-02 17:29:24 +0100

    examples for loop

---
+++ 1102_lecture_4/sketch_circle_2.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(800, 300);
+  }
+
+  function draw() {
+    background(1, 75, 100);
+    //circle properties
+    fill(237, 34, 93);
+    noStroke();
+    let diameter = 50;
+    
+    for (let i=0; i<width/diameter; i=i+1) {
+        for (let j=0; j<height/diameter; j=j+1) {
+            ellipse(
+                diameter/2 + i * diameter,
+                diameter/2 + j * diameter,
+                //diameter * random()
+                diameter
+            );
+        }
+    }
+      
+  }
+
+  //wir nutzen die random Funktion und multiplizieren die width der ellipse mit der random nummer
