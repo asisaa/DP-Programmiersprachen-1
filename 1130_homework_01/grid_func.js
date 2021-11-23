a35f9dfb3b5908d9788184ce3bc3a66560f000b4
Asisa Asseily <asisaasseily@asisas-mbp.lan>
2021-11-23 18:52:55 +0100

    Bonusaufgabe

---
+++ 1130_homework_01/grid_func.js
@@ -0,0 +1,25 @@
+
+/* 
+BONUS AUFGABE
+Erstelle eine Funktion die grid heißt und die drei Parametern hat.
+-> numX für die Anzahl an Shapes (z.B. ellipse) auf der x-Achse
+-> numY für die Anzahl an Shapes auf der y-Achse
+-> size für die größe der Shapes
+
+grid(10, 30, 20); // Erstellt 10 x 30 ellipsen von der size 20px
+ */
+
+
+function setup() {
+	createCanvas(800, 300);
+}
+
+function draw() {
+	background(220);
+    grid(5, 30, 20); //function call
+}
+//function declaration
+function grid (numX, numY, size) {
+
+
+}
