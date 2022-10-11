bb58ae70a576699adf778bbd16d3453a82051c90
Asisa Asseily <asisaasseily@asisas-mbp.lan>
2022-10-11 12:09:34 +0200

    update WS22_23

--- 2223_WS/22_0412_lecture_04/forloop.js
+++
@@ -1,9 +0,0 @@
-//example of a nested for loop
-//i++  its the same than i = i + 1;
-for (let i = 1; i < 2; i++) {
-  console.log("outer loop i: ", i);
-
-  for (let j = 1; j < 6; j++){
-    console.log("inner loop: ", j);
-  }
-}
