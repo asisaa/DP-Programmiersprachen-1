0072988c6386b25c183f293806512b3d97cfc4bc
Asisa Asseily <asisaasseily@Asisas-MacBook-Pro.local>
2022-10-18 10:29:21 +0200

    lecture2

---
+++ 2223_WS/22_1018_lecture_02/helloworld.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+    return 0;
+}
+
+/* In terminal, create a C++ file called hello.cpp: nano hello.cpp; Compile g++ hello.cpp
+Run with ./a.out on Mac the compiler is */
