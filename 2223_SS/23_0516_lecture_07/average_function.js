4efe635af35736c250a9510bcac40923ad204e85
Asisa Asseily <asisaasseily@asisas-mbp.lan>
2023-05-17 15:00:12 +0200

    added lecture07

---
+++ 2223_SS/23_0516_lecture_07/average_function.js
@@ -0,0 +1,16 @@
+let result;
+let result2;
+
+function average(x,y) { 
+	return (x + y)/2; 
+}
+
+//result = average(55, 66); 
+result2 = average('6','4');
+
+//console.log('result 1:', result); 
+console.log('result 2:',result2); 
+
+
+
+
